b64af540e47412a5b3c3fdd3252c69eeb78f047b
MonthAlcantara <montival_junior@yahoo.com.br>
2022-12-18 09:03:48 -0300

    first commit

---
+++ src/main/java/io/github/monthalcantara/githubactions/EstudoGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoGithubActionsApplication.class, args);
+	}
+
+}
